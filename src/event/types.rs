e7b000e003d70feea014aa00f613a74c4531c4c3
tami5 <kkharji@protonmail.com>
2022-02-20 19:25:45 +0300

    chore: add documentation to events

--- src/event/types.rs
+++ src/event/types.rs
@@ -7,21 +7,30 @@ use std::fmt::Debug;
 #[derive(Debug)]
 pub enum Event {
     NotSupported,
+    /// Mission Control specfic events
     MissionControl(MissionControlEvent),
+    /// Window specfic event
     Window(WindowEvent),
+    /// Display specfic events
     Display(DisplayEvent),
+    /// Space specfic events
     Space(SpaceEvent),
+    /// Application specfic events
     Application(ApplicationEvent),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum MissionControlEvent {
+    /// Triggered when mission-control activates.
     Enter,
+    /// Triggered when mission-control deactivates.
     Exit,
 }
 
 impl MissionControlEvent {
     /// Returns `true` if the mission control event is [`Enter`].
+    ///
+    /// i.e. did mission control activate?
     ///
     /// [`Enter`]: MissionControlEvent::Enter
     pub fn is_enter_event(&self) -> bool {
@@ -29,6 +38,8 @@ impl MissionControlEvent {
     }
 
     /// Returns `true` if the mission control event is [`Exit`].
+    ///
+    /// i.e. did mission control deactivates?
     ///
     /// [`Exit`]: MissionControlEvent::Exit
     pub fn is_exit_event(&self) -> bool {
@@ -38,11 +49,16 @@ impl MissionControlEvent {
 
 #[derive(Debug)]
 pub enum SpaceEvent {
+    /// Triggered when the active space has changed.
+    ///
+    /// Passes two arguments: $YABAI_SPACE_ID, $YABAI_RECENT_SPACE_ID
     Changed,
 }
 
 impl SpaceEvent {
     /// Returns `true` if the space event is [`Changed`].
+    ///
+    /// i.e. did active space change?
     ///
     /// [`Changed`]: SpaceEvent::Changed
     pub fn is_change_event(&self) -> bool {
@@ -52,15 +68,32 @@ impl SpaceEvent {
 
 #[derive(Debug)]
 pub enum DisplayEvent {
+    /// Triggered when a new display has been added.
+    ///
+    /// Passes one argument: $YABAI_DISPLAY_ID
     Added,
+    /// Triggered when a display has been removed.
+    ///
+    /// Passes one argument: $YABAI_DISPLAY_ID
     Removed,
+    /// Triggered when a change has been made to display arrangement.
+    ///
+    /// Passes one argument: $YABAI_DISPLAY_ID
     Moved,
+    /// Triggered when a display has changed resolution.
+    ///
+    /// Passes one argument: $YABAI_DISPLAY_ID
     Resized,
+    /// Triggered when the active display has changed.
+    ///
+    /// Passes two arguments: $YABAI_DISPLAY_ID, $YABAI_RECENT_DISPLAY_ID
     Changed,
 }
 
 impl DisplayEvent {
     /// Returns `true` if the display event is [`Added`].
+    ///
+    /// i.e. did a new display got attached?
     ///
     /// [`Added`]: DisplayEvent::Added
     pub fn is_add_event(&self) -> bool {
@@ -69,12 +102,16 @@ impl DisplayEvent {
 
     /// Returns `true` if the display event is [`Removed`].
     ///
+    /// i.e. did a display got removed?
+    ///
     /// [`Removed`]: DisplayEvent::Removed
     pub fn is_remove_event(&self) -> bool {
         matches!(self, Self::Removed)
     }
 
     /// Returns `true` if the display event is [`Moved`].
+    ///
+    /// i.e. did a display got moved?
     ///
     /// [`Moved`]: DisplayEvent::Moved
     pub fn is_move_event(&self) -> bool {
@@ -83,12 +120,16 @@ impl DisplayEvent {
 
     /// Returns `true` if the display event is [`Resized`].
     ///
+    /// i.e. did a display got resized?
+    ///
     /// [`Resized`]: DisplayEvent::Resized
     pub fn is_resize_event(&self) -> bool {
         matches!(self, Self::Resized)
     }
 
     /// Returns `true` if the display event is [`Changed`].
+    ///
+    /// i.e. did focus switch to different display
     ///
     /// [`Changed`]: DisplayEvent::Changed
     pub fn is_change_event(&self) -> bool {
@@ -98,18 +139,44 @@ impl DisplayEvent {
 
 #[derive(Debug)]
 pub enum WindowEvent {
+    /// Triggered when a window is created + implicitly created at application launch.
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Created,
+    /// Triggered when a window is destroyed + implicitly destroyed at application exit.
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Destroyed,
+    /// Triggered when a window becomes the key-window. +
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Focused,
+    /// Triggered when a window changes position. +
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Moved,
+    /// Triggered when a window changes dimensions.
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Resized,
+    /// Triggered when a window has been minimized. +
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Minimized,
+    /// Triggered when a window has been deminimized. +
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     Deminimized,
+    /// Triggered when a window changes its title. +
+    ///
+    /// Passes one argument: $YABAI_WINDOW_ID
     TitleChanged,
 }
 
 impl WindowEvent {
     /// Returns `true` if the window event is [`Created`].
+    ///
+    /// i.e. a new window created in a current space.
     ///
     /// [`Created`]: WindowEvent::Created
     pub fn is_create_event(&self) -> bool {
@@ -118,12 +185,16 @@ impl WindowEvent {
 
     /// Returns `true` if the window event is [`Destroyed`].
     ///
+    /// i.e. a window destroyed in a current space.
+    ///
     /// [`Destroyed`]: WindowEvent::Destroyed
     pub fn is_destory_event(&self) -> bool {
         matches!(self, Self::Destroyed)
     }
 
     /// Returns `true` if the window event is [`Focused`].
+    ///
+    /// i.e. a new window gained focus
     ///
     /// [`Focused`]: WindowEvent::Focused
     pub fn is_focus_event(&self) -> bool {
@@ -132,12 +203,16 @@ impl WindowEvent {
 
     /// Returns `true` if the window event is [`Moved`].
     ///
+    /// i.e. a window position has changed
+    ///
     /// [`Moved`]: WindowEvent::Moved
     pub fn is_move_event(&self) -> bool {
         matches!(self, Self::Moved)
     }
 
     /// Returns `true` if the window event is [`Resized`].
+    ///
+    /// i.e. a window has been resized
     ///
     /// [`Resized`]: WindowEvent::Resized
     pub fn is_resize_event(&self) -> bool {
@@ -146,6 +221,8 @@ impl WindowEvent {
 
     /// Returns `true` if the window event is [`Minimized`].
     ///
+    /// i.e. a window got minimized
+    ///
     /// [`Minimized`]: WindowEvent::Minimized
     pub fn is_minimize_event(&self) -> bool {
         matches!(self, Self::Minimized)
@@ -153,12 +230,16 @@ impl WindowEvent {
 
     /// Returns `true` if the window event is [`Deminimized`].
     ///
+    /// i.e. a window got deminimized
+    ///
     /// [`Deminimized`]: WindowEvent::Deminimized
     pub fn is_deminimize_event(&self) -> bool {
         matches!(self, Self::Deminimized)
     }
 
     /// Returns `true` if the window event is [`TitleChanged`].
+    ///
+    /// i.e. a window got title got updated.
     ///
     /// [`TitleChanged`]: WindowEvent::TitleChanged
     pub fn is_title_change_event(&self) -> bool {
@@ -168,17 +249,40 @@ impl WindowEvent {
 
 #[derive(Debug)]
 pub enum ApplicationEvent {
+    /// Triggered when a new application is launched.
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Launched,
+    /// Triggered when an application is terminated. +
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Terminated,
+    /// Triggered when the front-most application changes
+    ///
+    /// Passes two arguments: $YABAI_PROCESS_ID, $YABAI_RECENT_PROCESS_ID
     FrontSwitched,
+    /// Triggered when an application is activated.
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Activated,
+    /// Triggered when an application is deactivated.
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Deactivated,
+    /// Triggered when an application is unhidden.
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Visible,
+    /// Triggered when an application is hidden.
+    ///
+    /// Passes one argument: $YABAI_PROCESS_ID
     Hidden,
 }
 
 impl ApplicationEvent {
     /// Returns `true` if the application event is [`Launched`].
+    ///
+    /// i.e. a new application launched
     ///
     /// [`Launched`]: ApplicationEvent::Launched
     pub fn is_launch_event(&self) -> bool {
@@ -187,12 +291,16 @@ impl ApplicationEvent {
 
     /// Returns `true` if the application event is [`Terminated`].
     ///
+    /// i.e. an application got terminated.
+    ///
     /// [`Terminated`]: ApplicationEvent::Terminated
     pub fn is_terminate_event(&self) -> bool {
         matches!(self, Self::Terminated)
     }
 
     /// Returns `true` if the application event is [`FrontSwitched`].
+    ///
+    /// i.e. an application became ontop?
     ///
     /// [`FrontSwitched`]: ApplicationEvent::FrontSwitched
     pub fn is_front_switch_event(&self) -> bool {
@@ -201,12 +309,16 @@ impl ApplicationEvent {
 
     /// Returns `true` if the application event is [`Activated`].
     ///
+    /// i.e. an application got activated
+    ///
     /// [`Activated`]: ApplicationEvent::Activated
     pub fn is_activate_event(&self) -> bool {
         matches!(self, Self::Activated)
     }
 
     /// Returns `true` if the application event is [`Deactivated`].
+    ///
+    /// i.e. an application got deactivated
     ///
     /// [`Deactivated`]: ApplicationEvent::Deactivated
     pub fn is_deactivate_event(&self) -> bool {
@@ -215,12 +327,16 @@ impl ApplicationEvent {
 
     /// Returns `true` if the application event is [`Visible`].
     ///
+    /// i.e. an application got unhidden or made visible.
+    ///
     /// [`Visible`]: ApplicationEvent::Visible
     pub fn is_visible_event(&self) -> bool {
         matches!(self, Self::Visible)
     }
 
     /// Returns `true` if the application event is [`Hidden`].
+    ///
+    /// i.e. an application got hidden.
     ///
     /// [`Hidden`]: ApplicationEvent::Hidden
     pub fn is_hidden_event(&self) -> bool {
